075e479b9b8cf32f1915c1c6f96adb7466f65391
M0nenvoy <goldseeker68191@gmail.com>
2022-10-27 21:15:49 +0400

    New schemas and crud's

--- database/crud.py
+++ database/crud.py
@@ -52,8 +52,23 @@ def get_house_id_by_address(db: Session, address: str):
 
     return house.id
 
-def create_house_apartments(db: Session, house: schemas.HouseApartments, house_id: int):
-    db_item = models.HouseApartments(**house.dict(), house_id=house_id)
+
+def get_house_address_by_address_str(db: Session, address: str):
+    house = db.query(models.HouseAddress).filter(models.HouseAddress.address==address).first()
+    return house
+
+
+def get_house_address_by_id(db: Session, id: int):
+    return db.query(models.HouseAddress).filter(models.HouseAddress.id==id).first()
+
+
+def get_house_apartments_by_house_id(db: Session, house_id: int):
+    return db.query(models.HouseApartments).filter(models.HouseApartments.house_id==house_id).first()
+
+def create_house_apartments(db: Session, house: schemas.HouseApartmentsCreate):
+    db_item = models.HouseApartments(**house.dict())
     db.add(db_item)
     db.commit()
     db.refresh(db_item)
+
+    return db_item
