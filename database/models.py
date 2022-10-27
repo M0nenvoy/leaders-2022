36d201e68a19cbe9ca1e130e6821409d4699f028
M0nenvoy <goldseeker68191@gmail.com>
2022-10-27 21:31:45 +0400

    Stuff that makes our database go brrrr

--- database/models.py
+++ database/models.py
@@ -1,39 +1,20 @@
 # SQLAlchemy models
 
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, UniqueConstraint
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, ForeignKey, Integer, String, UniqueConstraint
 
 from database.session import Base
-
-
-class User(Base):
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
-    is_active = Column(Boolean, default=True)
-
-    items = relationship("Item", back_populates="owner")
-
-class Item(Base):
-    __tablename__ = "items"
-
-    id          = Column(Integer, primary_key=True, index=True)
-    title       = Column(String, index=True)
-    owner_id    = Column(Integer, ForeignKey("users.id"))
-
-    owner = relationship("User", back_populates="items")
 
 
 """
 Модель, хранящая строку с адресом какого-либо дома
 """
 class HouseAddress(Base):
-    __tablename__ = "house_adresses"
+    __tablename__ = "house_addresses"
 
     id      = Column(Integer, primary_key=True, index=True)
     address = Column(String)
+
+    UniqueConstraint("address")
 
 
 """
@@ -44,4 +25,6 @@ class HouseApartments(Base):
 
     id          = Column(Integer, primary_key=True, index=True)
     apartments  = Column(Integer)
-    house_id    = Column(Integer, ForeignKey("house_adresses.id"))
+    house_id    = Column(Integer, ForeignKey("house_addresses.id"))
+
+    UniqueConstraint("house_id")
