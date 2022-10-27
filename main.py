2f534126b4f2fcd6801886c299db71f1b6fd6c9f
M0nenvoy <goldseeker68191@gmail.com>
2022-10-27 21:27:26 +0400

    Cleaned up the things... That I actually used to learn alchemy and we don't really need

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from typing import List
-
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 
@@ -16,35 +14,42 @@ def get_db():
     finally:
         db.close()
 
-
-@app.post("/users/", response_model=schemas.User)
-def create_user (user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_email(db, user.email)
-    if db_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
-
-    return crud.create_user(db=db, user=user)
-
-
 @app.post("/create-house/", response_model=schemas.HouseAddress)
 def create_house (house: schemas.HouseAddressCreate, db: Session = Depends(get_db)):
-    house_address = crud.create_house_address(db, house)
-    # house_address = schemas.HouseAddress(address="Test address", id=1)
-    return house_address
+    # Сперва проверим, не существует ли уже дома с таким же адресом
+    house_db = crud.get_house_address_by_address_str(db, address=house.address)
+    if house_db:
+        raise HTTPException(status_code=400, detail="House with this address already exists")
+
+    return crud.create_house_address(db, house)
+
+
+@app.post("/create-house-apartments/", response_model=schemas.HouseApartments)
+def create_house_apartments(house: schemas.HouseApartmentsCreate, db: Session = Depends(get_db)):
+    # Сперва проверим, не существует ли уже дома с таким же house_id
+    house_apartments_db = crud.get_house_apartments_by_house_id(db, house.house_id)
+    if house_apartments_db:
+        raise HTTPException(status_code=400, detail="House with this house_id already exists")
+
+    # Проверим, существует ли дом с id=house_id
+    house_address_db = crud.get_house_address_by_id(db, house.house_id)
+    if house_address_db is None:
+        raise HTTPException(status_code=400, detail="No house with id={}. Can't reference with the foreign key".format(house.house_id))
+
+    return crud.create_house_apartments(db, house)
+
+
+@app.get("/get-house-apartments/{id}", response_model=schemas.HouseApartments)
+def get_house_apartments(id: int, db: Session = Depends(get_db)):
+    house_apartments = crud.get_house_apartments_by_house_id(db, id)
+    if house_apartments is None:
+        raise HTTPException(status_code=404, detail="House apartments with such id are not present")
+
+    return house_apartments.__dict__
 
 
 @app.get("/get-house/", response_model=int)
 def get_house (address: str = "Not specified", db: Session = Depends(get_db)):
-    return crud.get_house_id_by_address(db, address)
-
-
-@app.get("/users/", response_model=list[schemas.User])
-def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    users = crud.get_users(db, skip=skip, limit=limit)
-    return users
-
-
-@app.get("/users/{user_id}", response_model=schemas.User)
-def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id)
-    if db_user is None: raise HTTPException(status_code=404, detail="User with id (%d) not found".format(user_id))
+    import pdb; pdb.set_trace()
+    house = crud.get_house_id_by_address(db, address)
+    return house
